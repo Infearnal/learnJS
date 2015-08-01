8ba9fdf0d82cea3df4fcdcbe86d8bd01871cc174
romanchumychov <infearnal86@gmail.com>
2015-08-01 21:09:19 +0300

    JavaScript Boolean undefined null

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 function init() {
 
-    var str = "asdf лор дфр пдфывпр фдыв",
-        str2 = 'asdf',
-        str3 = "asdf \"sadf\" asgaetr",
-        longStr = "adfasdf \n \t string \t fuck \n asdg";
-    
-    // console.log(longStr);
-    // console.log(str3.length);
-    // console.log('hello ' + 'world');
-    // console.log('hello' .concat('world'));
-    // console.log(str.charAt(1));
-    // console.log(str.charCodeAt(1));
-    // console.log(str.substring(1, 3)); //обрезает с какого по какой символа
-    // console.log(str.slice(0, 4)); //обрезает с какого символа
-    // console.log(str.substr(8, 5 )); //обрезает с какого и сколько
-    // console.log(str.split(" ")); //разбивает на массив
-    // console.log(str.replace("лор", "fock")); //заменяет слова
-    // console.log(str.indexOf('a')); //выводит index символа
-    // console.log(str.toUpperCase()); //вверхний регистр
-    // console.log(str.toLowerCase()); //нижний регистр
+    var blabla,
+        obj = {},
+        arr = [1, 3 , 4];
+
+    //Boolean
+    //все true кроме пяти исключений
+    console.log( Boolean(NaN));
+    console.log( Boolean(''));
+    console.log( Boolean(0));
+    console.log( Boolean(undefined));
+    console.log( Boolean(null));
+
+    //null
+
+    //undefined - что то не обьявили или не передали
+    console.log(blabla);
+    console.log(obj.name);
+    console.log(arr[8]);
 }
 window.onload = init;
