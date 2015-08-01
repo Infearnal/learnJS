e6a26ea453a82cb5c5980fe33ebdf42ca9bf8e27
romanchumychov <infearnal86@gmail.com>
2015-08-01 22:36:03 +0300

    JavaScript data type

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 function init() {
 
-    var blabla,
-        obj = {},
-        arr = [1, 3 , 4];
+    console.log(typeof String(455) ); //
+    console.log(typeof Number('455') ); //
+    console.log(typeof Boolean(0)); //
 
-    //Boolean
-    //все true кроме пяти исключений
-    console.log( Boolean(NaN));
-    console.log( Boolean(''));
-    console.log( Boolean(0));
-    console.log( Boolean(undefined));
-    console.log( Boolean(null));
+    console.log(typeof (5 * '1')); //
 
-    //null
+    console.log(typeof (234234 + '')); //быстро в строку
+    console.log(typeof (+'123123')); //быстро в число
+    console.log(+''); //
+    console.log(+true); //
+    console.log(+false); //
+    console.log(typeof (!!'24')); //быстро в Boolean
 
-    //undefined - что то не обьявили или не передали
-    console.log(blabla);
-    console.log(obj.name);
-    console.log(arr[8]);
+    var num = 777;
+    console.log(typeof num.toString());
+
+    console.log(parseInt('100.1px')); //парсит число
+    console.log(parseFloat('123.14px')); //парсит число с плавающей точкой
 }
 window.onload = init;
