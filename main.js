51127b26412c6eb7f5ec6bb404c8869bc080dfae
romanchumychov <infearnal86@gmail.com>
2015-08-02 16:45:33 +0300

    JavaScript functions

--- main.js
+++ main.js
@@ -1,41 +1,88 @@
 function init() {
 
-    //LOOPS
-    //for
-    // for(/*выражение*/ инициализация; тест; инкримент(декримент)) {
-    //     //тело цикла
+    //function
+    // function имя(параметр) {
+    //     тело функции(инструкции);
     // }
+    // имя(аргумент);
+    function func(a, b) {
+        return a + b;
+    }
+    // console.log(func(2, 5));
 
-    // var arr = [1, 2, 3, 4],
-    //     sum = 0;
-    // for(var i = 0; i < arr.length; i++) {
-    //     sum = sum + arr[i];
-    // }
-    // console.log(sum);
+    //2 вида функций function declaration & function expration
+    //function declaration
+    function name() {
+        //body;
+    }
+    //function expration
+    var name = function() {
+        //body;
+    }
 
-    //for in - цикл по обьекту 
-    // var obj = {
-    //     prop: 'one',
-    //     name: 'Roman'
-    // }
-    // var prop;
-    // for(prop in obj) {
-    //     if(obj.hasOwnProperty(prop)) { // обязательная проверка
-    //         console.log(prop + ' : ' + obj[prop]);
+    //разница
+    // function func1() {
+    //     function funcExample() {
+    //         return 'one';
+    //     }
+
+    //     return funcExample();
+
+    //     function funcExample() {
+    //         return 'two';
     //     }
     // }
 
-    //while
-    // var k = 0;
-    // while(k < 10) {
-    //     console.log(k++);
+    // function func2() {
+    //     var funcExample = function() {
+    //         return 'one';
+    //     }
+
+    //     return funcExample();
+
+    //     var funcExample = function() {
+    //         return 'two';
+    //     }
     // }
+    // console.log(func1());
+    // console.log(func2());
 
-    //do while - 1 раз но обязательно выведется
-    var h = 0;
-    do {
-        console.log(h++);
-    } while (h < 10);
+    //функции обратного вызова 'callback'
+    // var func = function(callback) {
+    //     var name = 'Roman';
+    //     return callback(name);
+    // }
+    // console.log(func(function(p) {
+    //     return 'hello ' + p;
+    // }));
 
+    //возвращеник функции
+    // var func1 = function() {
+    //     return function() {
+    //         // console.log('hello world!');
+    //     };
+    // };
+    // func1()();
+
+    // ;(function(){ //анонимная самовызывающаяся функция
+    //     // console.log('anonimus)!');
+    // })();
+
+    // var prop = "fock";
+    // var module = (function(pro){ //патерн модуля
+    //     // console.log('anonimus)!');
+    //     // console.log(pro);
+    // })(prop);
+
+    var funcArg = function() { //обьект косящий под массив
+        var i,
+            sum = 0;
+        for(i = 0; i < arguments.length; i++) {
+            sum += arguments[i];
+        }
+        return sum;
+        // console.log(arguments); //есть свойство length
+    };
+    console.log(funcArg(1,2,3,4));
 }
 window.onload = init;
