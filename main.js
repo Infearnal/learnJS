fddb1e61e495ee0a24659a94d08ffbadab76c970
romanchumychov <infearnal86@gmail.com>
2015-08-02 17:28:36 +0300

    JavaScript scope

--- main.js
+++ main.js
@@ -1,88 +1,46 @@
 function init() {
 
-    //function
-    // function имя(параметр) {
-    //     тело функции(инструкции);
+    //SCOPE (global & local)
+    // var str = 'it\'s a string';
+
+    // function initName() {
+    //     var myName = 'Roman';
+    //     return myName;
     // }
-    // имя(аргумент);
-    function func(a, b) {
-        return a + b;
-    }
-    // console.log(func(2, 5));
+    // initName();
 
-    //2 вида функций function declaration & function expration
-    //function declaration
-    function name() {
-        //body;
-    }
-    //function expration
-    var name = function() {
-        //body;
-    }
-
-    //разница
-    // function func1() {
-    //     function funcExample() {
-    //         return 'one';
+//влияние на glabal variable
+    // var inner = function() {
+    //     var arr1 = [1,2,3];
+    //     for(i = 0; i < arr1.length; i++) { //надо перед i VAR 
+    //         console.log(arr1[i]);
+    //         console.log('first array');
+    //         outer();
     //     }
-
-    //     return funcExample();
-
-    //     function funcExample() {
-    //         return 'two';
-    //     }
-    // }
-
-    // function func2() {
-    //     var funcExample = function() {
-    //         return 'one';
-    //     }
-
-    //     return funcExample();
-
-    //     var funcExample = function() {
-    //         return 'two';
-    //     }
-    // }
-    // console.log(func1());
-    // console.log(func2());
-
-    //функции обратного вызова 'callback'
-    // var func = function(callback) {
-    //     var name = 'Roman';
-    //     return callback(name);
-    // }
-    // console.log(func(function(p) {
-    //     return 'hello ' + p;
-    // }));
-
-    //возвращеник функции
-    // var func1 = function() {
-    //     return function() {
-    //         // console.log('hello world!');
-    //     };
     // };
-    // func1()();
+    // var outer = function() {
+    //     var arr2 = [4,5,6];
+    //     for(i = 0; i < arr2.length; i++) {//надо перед i VAR
+    //         console.log(arr2[i]);
+    //         console.log('second array');
+    //     }
+    // };
+    // inner();
 
-    // ;(function(){ //анонимная самовызывающаяся функция
-    //     // console.log('anonimus)!');
-    // })();
-
-    // var prop = "fock";
-    // var module = (function(pro){ //патерн модуля
-    //     // console.log('anonimus)!');
-    //     // console.log(pro);
-    // })(prop);
-
-    var funcArg = function() { //обьект косящий под массив
-        var i,
-            sum = 0;
-        for(i = 0; i < arguments.length; i++) {
-            sum += arguments[i];
+//цепочка областей видимости
+    var k = 4;
+    var outerScope = function() {
+        console.log(k);
+        var k = 8;
+        console.log(k);
+        var innerScope = function() {
+            console.log(k);
+            var k = 12;
         }
-        return sum;
-        // console.log(arguments); //есть свойство length
-    };
-    console.log(funcArg(1,2,3,4));
+        innerScope();
+        console.log(k);
+    }
+    outerScope();
+
 }
-window.onload = init;
+window.onload = init; 
