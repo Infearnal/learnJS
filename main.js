a3e3d85379810511d256dc7bc8dc210bc6c49f86
romanchumychov <infearnal86@gmail.com>
2015-08-03 13:30:51 +0300

    JavaScript DOM Document Object Model

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-// function init() {
- 
-     var foo = 'str';
-     console.log(foo);
+(function() {
 
-     console.log(navigator.userAgent);
-     console.log(navigator.platform);
+    var elem = document.getElementsByTagName('p'), //Node list (array)
+        classElem = document.getElementsByClassName('paragraph'),
+        idElem = document.getElementById('four'),
+        elemSelector = document.querySelector('p'), //view first elem
+        elemSelectors = document.querySelectorAll('p'), //all elem - getElementsByTagName работает быстрее
+        elemInDiv = document.querySelectorAll('div p'),
+        idElemSelector = document.querySelector('#four');
 
-     console.log(screen.width +  " x " +  screen.height);
+        // console.log(idElemSelector);
+        // console.log(idElem);
+        // console.log(classElem);
+    // console.log(elem);
+    for(var i = 0; i < elem.length; i++) {
+        // console.log(elem[i].tagName); //работа с елементами
+        // console.log(elem[i].parentNode);
+        // console.log(elem[i].previousSibling.previousSibling); //предыдущий родственник (2 раза так как считает пробельные ноды)
+        // console.log(elem[i].nextSibling.nextSibling);
+        // console.log(elem[i].nodeName); //работа с любыми узлами
+        // console.log(elem[i].nodeType); 
+    }
 
-     // console.log(location.reload()); //перезагружает браузер
-     // console.log(location);
-     // console.log(location.toString());
-     // console.log(location.href);
+    // console.log(document.querySelector('div').childNodes);
+    // console.log(document.querySelector('div').children);
+    // console.log(document.querySelector('div').lastChild);
+    // console.log(document.querySelector('div').firstChild);
+    // console.log(document.querySelector('div').innerHTML);
 
-     // console.log(frames);
-
-     console.log(history);
-
-// }
-// window.onload = init; 
+})();
