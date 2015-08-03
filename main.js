5a50fd023af45459b6a7de01484f32aa25044f30
romanchumychov <infearnal86@gmail.com>
2015-08-03 12:23:22 +0300

    JavaScript closure

--- main.js
+++ main.js
@@ -1,46 +1,22 @@
 function init() {
 
-    //SCOPE (global & local)
-    // var str = 'it\'s a string';
+    //замыкание - closure
 
-    // function initName() {
-    //     var myName = 'Roman';
-    //     return myName;
-    // }
-    // initName();
+    var firstFunc = function() {
+        var index = 5;
 
-//влияние на glabal variable
-    // var inner = function() {
-    //     var arr1 = [1,2,3];
-    //     for(i = 0; i < arr1.length; i++) { //надо перед i VAR 
-    //         console.log(arr1[i]);
-    //         console.log('first array');
-    //         outer();
-    //     }
-    // };
-    // var outer = function() {
-    //     var arr2 = [4,5,6];
-    //     for(i = 0; i < arr2.length; i++) {//надо перед i VAR
-    //         console.log(arr2[i]);
-    //         console.log('second array');
-    //     }
-    // };
-    // inner();
-
-//цепочка областей видимости
-    var k = 4;
-    var outerScope = function() {
-        console.log(k);
-        var k = 8;
-        console.log(k);
-        var innerScope = function() {
-            console.log(k);
-            var k = 12;
+        return function() {
+            return index;
         }
-        innerScope();
-        console.log(k);
     }
-    outerScope();
+
+    var secondFunc = function() {
+        var index = 15;
+
+        console.log(firstFunc()());
+    }
+
+    secondFunc(); //returned 5
 
 }
 window.onload = init; 
